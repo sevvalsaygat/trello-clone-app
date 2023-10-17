1b2d60cdf02081d1e836626df30744d1034dc370
sevvalsaygat <sevvallsstt@gmail.com>
2023-10-17 13:52:43 +0300

    fix: hydration error

--- app/boards/page.tsx
+++ app/boards/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import { useAuth } from "@app/hooks";
+import { useEffect } from "react";
 
 export default function Page() {
   const isAuthenticated = useAuth((state) => state.isAuthenticated);
   const currentUser = useAuth((state) => state.currentUser);
+
+  useEffect(() => {
+    useAuth.persist.rehydrate();
+  }, []);
 
   return (
     <div>{isAuthenticated && <div>Welcome {currentUser?.fullName}</div>}</div>
