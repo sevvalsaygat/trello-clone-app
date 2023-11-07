52b08718c8d86d004afe125158c0f7204a1ac031
sevvalsaygat <sevvallsstt@gmail.com>
2023-11-08 02:59:34 +0300

    feat: add dropdown to travel workspaces

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
 
 import { FormProvider, useForm } from "react-hook-form";
@@ -27,6 +28,14 @@ export default function Home() {
     { value: "operations", label: "Operations" },
     { value: "sales CRM", label: "Sales CRM" },
   ];
+
+  const [isActive, setIsActive] = useState(true);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+
+  const [isClosed, setIsClosed] = useState(false);
 
   return (
     <AppLayout>
@@ -73,7 +82,10 @@ export default function Home() {
               </div>
               <div role="travel" className="flex flex-col">
                 <div className="flex flex-col">
-                  <div className="flex flex-row justify-between hover:bg-blue-500 rounded-8 transition-all duration-100 cursor-pointer mb-1 w-full">
+                  <button
+                    onClick={handleClick}
+                    className="flex flex-row justify-between hover:bg-blue-500 rounded-8 transition-all duration-100 cursor-pointer mb-1 w-full"
+                  >
                     <div className="flex flex-row px-3 py-6 gap-2 items-center">
                       <div className="flex justify-center items-center w-6 h-6 border-transparent rounded-4 font-bold text-white text-14 bg-gradient-to-t from-blue-550 from-10% via-purple-150 via-70% to-purple-100 to-90%">
                         T
@@ -83,85 +95,97 @@ export default function Home() {
                       </div>
                     </div>
                     <div className="flex relative items-center mr-3">
-                      <Icons.SvgArrowUp className="text-slate-100 h-4 w-4 absolute" />
-                      <Icons.SvgArrowDown className="text-slate-100 h-4 w-4" />
+                      {isActive ? (
+                        <Icons.SvgArrowUp className="text-slate-100 h-4 w-4" />
+                      ) : (
+                        <Icons.SvgArrowDown className="text-slate-100 h-4 w-4" />
+                      )}
                     </div>
-                  </div>
-                  <div className="flex flex-row items-center py-6 pl-11 pr-2 gap-3 cursor-pointer bg-blue-600 rounded-8 mb-1">
-                    <Icons.SvgBoardTrello className="text-slate-100" />
-                    <div className="text-blue-400 text-14 leading-5">
-                      Boards
-                    </div>
-                  </div>
-                  <div className="flex flex-row items-center py-6 pl-11 pr-2 gap-3 cursor-pointer hover:bg-blue-500 rounded-8 transition-all duration-100 mb-1">
-                    <Icons.SvgHeart className="text-slate-100 w-4 h-4" />
-                    <div className="text-blue-300 text-14 leading-5">
-                      Highlights
-                    </div>
-                  </div>
-                  <div className="group/item flex flex-row items-center justify-between py-6 pl-11 pr-1 cursor-pointer hover:bg-blue-500 rounded-8 transition-all duration-100 mb-1">
-                    <div className="flex flex-row items-center gap-3">
-                      <Icons.SvgView className="text-slate-100 w-4 h-4" />
-                      <div className="text-blue-300 text-14 leading-5">
-                        View
+                  </button>
+                  {isActive && (
+                    <div>
+                      <div className="flex flex-row items-center py-6 pl-11 pr-2 gap-3 cursor-pointer bg-blue-600 rounded-8 mb-1">
+                        <Icons.SvgBoardTrello className="text-slate-100" />
+                        <div className="text-blue-400 text-14 leading-5">
+                          Boards
+                        </div>
+                      </div>
+                      <div className="flex flex-row items-center py-6 pl-11 pr-2 gap-3 cursor-pointer hover:bg-blue-500 rounded-8 transition-all duration-100 mb-1">
+                        <Icons.SvgHeart className="text-slate-100 w-4 h-4" />
+                        <div className="text-blue-300 text-14 leading-5">
+                          Highlights
+                        </div>
+                      </div>
+                      <div className="group/item flex flex-row items-center justify-between py-6 pl-11 pr-1 cursor-pointer hover:bg-blue-500 rounded-8 transition-all duration-100 mb-1">
+                        <div className="flex flex-row items-center gap-3">
+                          <Icons.SvgView className="text-slate-100 w-4 h-4" />
+                          <div className="text-blue-300 text-14 leading-5">
+                            View
+                          </div>
+                        </div>
+                        <div className="mr-2">
+                          <Icons.SvgArrowRight className="hidden group-hover/item:block text-slate-100 w-4 h-4 " />
+                        </div>
+                      </div>
+                      <div className="group/item flex flex-row items-center justify-between py-6 pl-11 pr-1 cursor-pointer hover:bg-blue-500 rounded-8 transition-all duration-100 mb-1">
+                        <div className="flex flex-row items-center gap-3">
+                          <Icons.SvgMember className="text-slate-100 w-4 h-4" />
+                          <div className="text-blue-300 text-14 leading-5">
+                            Members
+                          </div>
+                        </div>
+                        <div className="flex flex-row relative gap-3">
+                          <div className="ml-5">
+                            <Icons.SvgPlus className="text-slate-100 w-4 h-4" />
+                          </div>
+                          <div className="mr-2">
+                            <Icons.SvgArrowRight className="hidden group-hover/item:block text-slate-100 w-4 h-4 " />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="group/item flex flex-row items-center justify-between py-6 pl-11 pr-1 cursor-pointer hover:bg-blue-500 rounded-8 transition-all duration-100 mb-1">
+                        <div className="flex flex-row items-center gap-3">
+                          <Icons.SvgSetting className="text-slate-100 w-4 h-4" />
+                          <div className="text-blue-300 text-14 leading-5">
+                            Settings
+                          </div>
+                        </div>
+                        <div className="mr-2">
+                          <Icons.SvgArrowRight className="hidden group-hover/item:block text-slate-100 w-4 h-4 " />
+                        </div>
                       </div>
                     </div>
-                    <div className="mr-2">
-                      <Icons.SvgArrowRight className="hidden group-hover/item:block text-slate-100 w-4 h-4 " />
-                    </div>
-                  </div>
-                  <div className="group/item flex flex-row items-center justify-between py-6 pl-11 pr-1 cursor-pointer hover:bg-blue-500 rounded-8 transition-all duration-100 mb-1">
-                    <div className="flex flex-row items-center gap-3">
-                      <Icons.SvgMember className="text-slate-100 w-4 h-4" />
-                      <div className="text-blue-300 text-14 leading-5">
-                        Members
-                      </div>
-                    </div>
-                    <div className="flex flex-row relative gap-3">
-                      <div className="ml-5">
-                        <Icons.SvgPlus className="text-slate-100 w-4 h-4" />
-                      </div>
-                      <div className="mr-2">
-                        <Icons.SvgArrowRight className="hidden group-hover/item:block text-slate-100 w-4 h-4 " />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="group/item flex flex-row items-center justify-between py-6 pl-11 pr-1 cursor-pointer hover:bg-blue-500 rounded-8 transition-all duration-100 mb-1">
-                    <div className="flex flex-row items-center gap-3">
-                      <Icons.SvgSetting className="text-slate-100 w-4 h-4" />
-                      <div className="text-blue-300 text-14 leading-5">
-                        Settings
-                      </div>
-                    </div>
-                    <div className="mr-2">
-                      <Icons.SvgArrowRight className="hidden group-hover/item:block text-slate-100 w-4 h-4 " />
-                    </div>
-                  </div>
+                  )}
                 </div>
-                <div className="premium-card border-y border-blue-500 rounded-3">
-                  <div className="px-3">
-                    <div className="flex flex-row justify-between items-center mt-4 mb-2">
-                      <div className="text-blue-300 text-14 leading-4 font-medium">
-                        Try Trello Premium
+                {!isClosed && (
+                  <div className="premium-card border-y border-blue-500 rounded-3">
+                    <div className="px-3">
+                      <div className="flex flex-row justify-between items-center mt-4 mb-2">
+                        <div className="text-blue-300 text-14 leading-4 font-medium">
+                          Try Trello Premium
+                        </div>
+                        <button
+                          onClick={() => setIsClosed(true)}
+                          className="rounded-3 hover:bg-blue-500"
+                        >
+                          <Icons.SvgClose className="text-slate-100 h-4 w-4" />
+                        </button>
                       </div>
-                      <button className="rounded-3 hover:bg-blue-500">
-                        <Icons.SvgClose className="text-slate-100 h-4 w-4" />
-                      </button>
-                    </div>
-                    <div className="text-14 leading-5 text-slate-100 mb-4">
-                      Get unlimited boards, all the views, unlimited automation,
-                      and more.
-                    </div>
-                    <div className="flex flex-row justify-between mb-4">
-                      <button className="text-slate-100 hover:text-blue-300 text-14 leading-5 underline">
-                        Start free trial
-                      </button>
-                      <div className="rounded-3 bg-purple-200 p-1">
-                        <Icons.SvgPremium className="text-white" />
+                      <div className="text-14 leading-5 text-slate-100 mb-4">
+                        Get unlimited boards, all the views, unlimited
+                        automation, and more.
+                      </div>
+                      <div className="flex flex-row justify-between mb-4">
+                        <button className="text-slate-100 hover:text-blue-300 text-14 leading-5 underline">
+                          Start free trial
+                        </button>
+                        <div className="rounded-3 bg-purple-200 p-1">
+                          <Icons.SvgPremium className="text-white" />
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
+                )}
               </div>
             </div>
           </div>
